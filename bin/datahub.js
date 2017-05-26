42266f7e8e0aca8bafcd75774ecf12925852e63e
Irakli Mchedlishvili <zelima@users.noreply.github.com>
2017-05-26 13:19:24 +0400

    Read config (#18)
    [commands][m]:
    - Improvements for `datahub configure` command
    - function for reading config
    - new fixtures and test
    refs #12

--- bin/datahub.js
+++ bin/datahub.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node --harmony
-const configure = require('../lib/config.js').configure
+const config = require('../lib/config.js')
 const get = require('../lib/index.js').get
 const program = require('commander')
 const version = require('../package.json').version
@@ -7,6 +7,7 @@ const version = require('../package.json').version
 program
   .version(version)
   .usage('<command> [options] ')
+  .option('-c, --config <path>', 'Use custom config file', config.configFile)
 
 program
   .command('get <package> [dest]')
@@ -20,7 +21,7 @@ program
   .command('configure [dest]')
   .description('Set Up DataHub Configurations')
   .action(function(dest) {
-    configure()
+    config.configure(program.config)
   })
 
 program.parse(process.argv)
