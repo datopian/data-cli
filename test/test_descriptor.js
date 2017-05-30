5db411a9071dd5f99ebe28fd46af392ac9529455
Meiran Zhiyenbayev <meiran1991@gmail.com>
2017-05-30 13:08:22 +0600

    [generate][m]: Generated descriptor.json from inline csv - refs #10 (#24)
    * Created function for generating descriptor from tabular data.
    * Includes unittest.

---
+++ test/test_descriptor.js
@@ -0,0 +1,36 @@
+const assert = require('assert')
+const generateDescriptor = require('../lib/descriptor.js').generateDescriptor
+
+describe('generate dp', () => {
+  it('from inline data', function() {
+    const csvData = "a,b,c\n1,1991-01-13,test1\n4,1990-01-06,test2"
+    const expectedSchema = { 
+      fields: [ 
+        { 
+          name: 'a',
+          title: '',
+          description: '',
+          type: 'integer',
+          format: 'default'
+        },
+        { 
+          name: 'b',
+          title: '',
+          description: '',
+          type: 'date',
+          format: 'default'
+        },
+        { 
+          name: 'c',
+          title: '',
+          description: '',
+          type: 'string',
+          format: 'default'
+        }
+      ]
+     }
+    const out = generateDescriptor(csvData).then(val => {
+      assert.deepEqual(val.resources[0]['schema'], expectedSchema)
+    })
+  })
+})
