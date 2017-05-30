89b384d300c4eb506ac97db362e44b644dfe3cd7
mikanebu <meiran1991@gmail.com>
2017-05-30 17:37:56 +0600

    [generate-dp][l]: created function to generate dp from local files -  refs #10

--- test/test_descriptor.js
+++ test/test_descriptor.js
@@ -1,5 +1,8 @@
 const assert = require('assert')
 const generateDescriptor = require('../lib/descriptor.js').generateDescriptor
+const generateDescriptorForLocalFiles = require('../lib/descriptor.js').generateDescriptorForLocalFiles
+var mock = require('mock-fs');
+const get = require('../lib/index.js').get
 
 describe('generate dp', () => {
   it('from inline data', function() {
@@ -32,5 +35,120 @@ describe('generate dp', () => {
     const out = generateDescriptor(csvData).then(val => {
       assert.deepEqual(val.resources[0]['schema'], expectedSchema)
     })
+  })
+})
+
+describe('generate datapackage.json from local files: single file', () => {
+  it('check for csv data schema', function() { 
+    mock({
+      'data and Data and data and Data.csv': 'a,b,c\n1,1991-01-01,test1'
+    });
+    const expectedSchema = 
+    { 
+      name: 'scratchpad',
+      resources: [
+        {
+          path: 'data and Data and data and Data.csv',
+          name: 'data-and-data-and-data-and-data',
+          format: 'csv',
+          schema: {
+            fields: [
+              {
+                name: 'a',
+                title: '',
+                description: '',
+                type: 'integer',
+                format: 'default'
+              },
+              {
+                name: 'b',
+                title: '',
+                description: '',
+                type: 'date',
+                format: 'default'
+              },
+              {
+                name: 'c',
+                title: '',
+                description: '',
+                type: 'string',
+                format: 'default'
+              }
+            ]
+          }
+        }
+      ]
+    }
+    
+    const out = generateDescriptorForLocalFiles().then(val => {
+      assert.deepEqual(val, expectedSchema)
+      mock.restore();
+    })  
+  })
+})
+
+describe('generate datapackage.json from local files: multiple files', () => {
+  it('check for multiple files', function() { 
+    mock({
+      'dataJSON.json': '{"name":"datapackaed.com","messages":["test 1","test 2","test 3"],"status":100}',
+      'dataGEOJSON.geojson': '{ "type": "Point", "coordinates": [0, 0] }',
+      'data-TOPOJSON.topojson': '{"type": "LineString", "arcs": [42]}',
+      'testCSV CSV.csv': 'a,b,c\n1,1991-01-01,test1'
+    });
+    const expectedSchema = 
+    { 
+      name: 'scratchpad',
+      resources: [
+        {
+          path: 'data-TOPOJSON.topojson',
+          name: 'data-topojson',
+          format: 'topojson'
+        },
+        {
+          path: 'dataGEOJSON.geojson',
+          name: 'datageojson',
+          format: 'geojson'
+        },
+        {
+          path: 'dataJSON.json',
+          name: 'datajson',
+          format: 'json'
+        },
+        {
+          path: 'testCSV CSV.csv',
+          name: 'testcsv-csv',
+          format: 'csv',
+          schema: {
+            fields: [
+              {
+                name: 'a',
+                title: '',
+                description: '',
+                type: 'integer',
+                format: 'default'
+              },
+              {
+                name: 'b',
+                title: '',
+                description: '',
+                type: 'date',
+                format: 'default'
+              },
+              {
+                name: 'c',
+                title: '',
+                description: '',
+                type: 'string',
+                format: 'default'
+              }
+            ]
+          }
+        }]
+    }
+    
+    const out = generateDescriptorForLocalFiles().then(val => {
+      assert.deepEqual(val, expectedSchema)
+      mock.restore();
+    })  
   })
 })
