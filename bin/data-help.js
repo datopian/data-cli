a0ac56c52e45b48c2b8c59f6f6a994774dee77b4
Irakli Mchedlishvili <zelima@users.noreply.github.com>
2017-06-01 09:28:35 +0400

    Basic now like cli (#29)
    * [cli][s]: basic now-like cli arg parser. displays help message -refs #28
    
    * [test][m]: tests set up and basic test for help command -refs #28

---
+++ bin/data-help.js
@@ -0,0 +1,6 @@
+// const chalk = require('chalk')
+
+console.log('\n  Welcome to the DataHub command line tool.')
+console.log('\n  DataHub = 📦  + 🐘  A home for all your data, nicely packaged ❒ ')
+console.log('\n  We hope this tool will bring you much joy as you work with your data and the DataHub.')
+console.log('\n  ---')
