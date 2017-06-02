f91912ec0ca7987a52e1888597268c15587094a0
zelima <irakli.mchedlishvili@gmail.com>
2017-06-02 09:45:51 +0400

    [cli][s]: main help message updated. empty file removed from lib

--- bin/data-help.js
+++ bin/data-help.js
@@ -2,7 +2,26 @@
 
 const { box, elephant, square } = require('../lib/utils/logo')
 
-console.log(`\n  Welcome to the DataHub command line tool.
-\n  DataHub = ${box}  + ${elephant}  A home for all your data, nicely packaged ${square}
-\n  We hope this tool will bring you much joy as you work with your data and the DataHub.
-\n  ---`)
+console.log(`
+  Welcome to the DataHub command line tool.
+
+  DataHub = ${box}  + ${elephant}  A home for all your data, nicely packaged ${square}
+
+  We hope this tool will bring you much joy as you work with your data and the DataHub.
+
+  ---
+
+  Usage: data <command> [options]
+
+  Commands:
+
+    get      <package>    View or Download file from DataHub
+    push                  Publish on DataHub server
+    config                Set Up DataHub Configurations
+    help                  Show help
+
+  Options:
+
+    -h,  --help           output usage information
+    -v,  --version        output the version number
+`)
