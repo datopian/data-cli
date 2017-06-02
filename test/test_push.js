01cbc48eb86716c2742fba8508ce5a63920bb9ef
zelima <irakli.mchedlishvili@gmail.com>
2017-06-02 15:13:25 +0400

    [logic:push][s]: get token from secret - refs #19

---
+++ test/test_push.js
@@ -0,0 +1,24 @@
+const test = require('ava')
+const push = require('../lib/push')
+const nock = require('nock')
+
+let config = {
+  username: 'test',
+  secretToken: 'secret',
+  server: 'https://test.com'
+}
+
+const postToken = nock(config.server)
+      .persist()
+      .post('/api/auth/token', {
+        username: config.username,
+        secret: config.secretToken
+      })
+      .reply(200, { token: 't35tt0k3N' })
+
+
+test('Gets the token', async t => {
+  const token = await push.getToken(config)
+  const expToken = 't35tt0k3N'
+  t.is(token, expToken)
+})
