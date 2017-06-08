46e97b2a8344716373e013d8b42ea083cc2304d2
anuveyatsu <anuar.ustayev@gmail.com>
2017-06-08 12:31:28 +0600

    [setup][xs]: refactoring so data function is a module

---
+++ test/data.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const { spawn } = require('cross-spawn')
+
+const data = (...args) => {
+  return new Promise((resolve, reject) => {
+    const command = path.resolve(__dirname, '../bin/data.js')
+    const data = spawn(command, args)
+
+    let stdout = ''
+    data.stdout.on('data', data => {
+      stdout += data
+    })
+
+    data.on('error', err => {
+      reject(err)
+    })
+
+    data.on('close', code => {
+      resolve({
+        code,
+        stdout
+      })
+    })
+  })
+}
+
+module.exports.data = data
