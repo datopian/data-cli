23e7681acdda017d2db53adc80275cf7d8725995
anuveyatsu <anuar.ustayev@gmail.com>
2017-06-12 11:35:48 +0600

    [purge][s]: refactored purge command with prompt - refs #38

--- test/test_purge.js
+++ test/test_purge.js
@@ -3,6 +3,7 @@ require("babel-polyfill")
 const test = require('ava')
 const purge = require('../lib/purge')
 const nock = require('nock')
+const sinon = require('sinon')
 
 let config = {
   username: 'test',
@@ -15,8 +16,22 @@ const postAuthorize = nock(config.server, {reqheaders : {"auth-token": "t35tt0k3
       .delete('/api/package/test/package/purge')
       .reply(200, { status: 'OK' })
 
+test.beforeEach(t => {
+  t.context.error = console.error
+  t.context.log = console.log
+  console.error = sinon.spy()
+  console.log = sinon.spy()
+})
+
 test('Returns status OK if purge is ok', async t => {
   const res = await purge.requestPurge(config,'package','t35tt0k3N')
   const exp = {status: 'OK'}
   t.deepEqual(res, exp)
+})
+
+test('Prompt works ok', async t => {
+  const dpjson = {'name': 'test'}
+  const res = purge.purgePermission(dpjson)
+  t.true(console.log.calledOnce)
+  t.true(console.log.firstCall.args[0].includes('Please, type in the name of the package to confirm'))
 })
