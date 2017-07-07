491eb04b346519a18bc8bf7023030867cffd5717
Anuar Ustayev <anuar.ustayev@gmail.com>
2017-07-07 10:07:10 +0200

    Refactor/data push package (#94)
    * [bin][xs]: removed obselete code - refs #92
    
    * [config][xs]: removed prompting secret token - refs #92
    
    * [utils][xs]: added module and test for checking if user is authenticated - refs #92
    
    * [push][s]: refactoring push command per new api - refs #92
    
    * removed getting token method in push command
    * check if user's token is valid and proceed, if not abort and ask to login
    * refactored getFileInfo function - renamed size with length
    * refactored getFileData (authorize) function - token is inside config now, modified api endpoint
    * refactored tests accordingly
    
    * [config][s]: refactored config and login module - refs #92
    
    * added new property api into config which defaults to http://api.datapackaged.com
    * refactored lib/login.js - replaced hardcoded url with api url from config
    * refactored lib/push.js accordingly
    
    * [tests][xs]: fixed tests after refactorings - refs #92

--- bin/data-push.js
+++ bin/data-push.js
@@ -2,18 +2,11 @@
 
 // Packages
 const minimist = require('minimist')
-const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
 const {customMarked} = require('../lib/utils/tools.js')
 // ours
 const { push } = require('../lib/push')
-const { box, elephant, square } = require('../lib/utils/logo')
-
-const dhStyle = chalk.bold.underline
-const italic = chalk.italic
-const boldText = chalk.bold
-const underline = chalk.underline
 
 const argv = minimist(process.argv.slice(2), {
   string: ['push'],
