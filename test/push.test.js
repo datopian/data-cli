ff85ec362e57ebd939b4b5c805ee85ba0d41b6d7
Rufus Pollock <rufus.pollock@okfn.org>
2017-07-10 14:19:22 +0200

    [push,#92,#98][m]: 95% of port of old push to new DataHub class push.
    * Lots of improvements esp in tests + simpler code
    * Main remaining of old code is the wiring into the push cli
    * Also have quite a bit of additional work of items that weren't even in existing code.

--- test/push.test.js
+++ test/push.test.js
@@ -183,27 +183,3 @@ test('Gets correct file info for request', t => {
   t.deepEqual(exp, res)
 })
 
-test('"data help push" prints help message for push command', async t => {
-  const result = await data('help', 'push')
-
-  t.is(result.code, 0)
-  const stdout = result.stdout.split('\n')
-  t.true(stdout.length > 1)
-  t.true(stdout[1].includes('Push a Data Package to DataHub'))
-})
-
-test('"data push -h --help" prints help message for push command', async t => {
-  let result = await data('push', '-h')
-
-  t.is(result.code, 0)
-  let stdout = result.stdout.split('\n')
-  t.true(stdout.length > 1)
-  t.true(stdout[1].includes('Push a Data Package to DataHub'))
-
-  result = await data('push', '--help')
-
-  t.is(result.code, 0)
-  stdout = result.stdout.split('\n')
-  t.true(stdout.length > 1)
-  t.true(stdout[1].includes('Push a Data Package to DataHub'))
-})
--- test/utils.datahub.test.js
+++ test/utils.datahub.test.js
@@ -66,32 +66,24 @@ const apiAuthorize = nock(config.api, {reqheaders : {"Auth-Token": "t35tt0k3N"}}
 const qs = '?key=...&policy=...&x-amz-algorithm=AWS4-HMAC-SHA256&x-amz-credential=XXX&x-amz-signature=YYY'
 const rawstoreMock = nock(rawstoreUrl, {
   }).post('/' + qs,
-    // TODO: get this working
-    // {
-    // body: '{\n  "name": "dp-no-resources",\n  "title": "DP with No Resources",\n  "resources": []\n}'
-  // }
+    "{\n  \"name\": \"dp-no-resources\",\n  \"title\": \"DP with No Resources\",\n  \"resources\": []\n}"
   ).reply(200)
 
 const apiSpecStore = nock(config.api, {
   reqheaders: {
     "Auth-Token": "t35tt0k3N"
   }
-}).persist()
-  .post('/source/upload', {
+  }).post('/source/upload', {
     "meta": {
       "version": 1,
-      "owner": "test",
-      "id": "<id>"
+      "owner": undefined
     },
-    "inputs": [
+    inputs: [
       {
-        "kind": "datapackage",
-        "url": "http://test.com/hash1",
-        "parameters": {
-          "resource-mapping": {
-            "file1": "http://test.com/hash2",
-            "file2": "http://test.com/hash3"
-          }
+        kind: 'datapackage',
+        url: 'https://s3-us-west-2.amazonaws.com/',
+        parameters: {
+          "resource-mapping": {}
         }
       }
     ]
@@ -111,119 +103,9 @@ test('push works with packaged dataset', async t => {
 
   t.is(apiAuthorize.isDone(), true)
   t.is(rawstoreMock.isDone(), true)
+  t.is(apiSpecStore.isDone(), true)
 
   // TODO: make sure we have not altered the pkg.resources object in any way
   t.is(pkg.resources.length, 0)
 })
 
-
-
-
-/*
-test('Gets correct file info for regular file', t => {
-  const fileInfo = push.getFileInfo('test/fixtures/sample.csv')
-  const exp = {
-    md5: "sGYdlWZJioAPv5U2XOKHRw==",
-    name: "test/fixtures/sample.csv",
-    length: 46,
-    type: "binary/octet-stream",
-  }
-  t.deepEqual(fileInfo, exp)
-})
-
-test('Gets correct file info for json file', t => {
-  const fileInfo = push.getFileInfo('test/fixtures/datapackage.json')
-  t.deepEqual(fileInfo, dpinfo)
-})
-*/
-
-/*
-test('Gets File data (authorize)', async t => {
-  const fileInfo = {
-    metadata: {
-        // TODO: owner?
-        owner: '',
-        name: 'test'
-    },
-    filedata: {'datapackage.json': dpinfo}
-  }
-  const exp = {
-    'datapackage.json': {
-      'md5-hash': "s0mEhash#",
-      'name': 'datapackage.json',
-      'type': 'application/json',
-      'upload_query': {},
-      'upload_url': 'https://s3-us-west-2.amazonaws.com/bits-staging.datapackaged.com'
-    }
-  }
-  const fileData = await datahub.rawstoreAuthorize(fileInfo)
-  t.deepEqual(fileData, exp)
-})
-*/
-
-/*
-test('Gets correct file list', t => {
-  const exp = ['datapackage.json', 'README.md', 'dp/ppp-gdp.csv']
-  const res = push.getFileList(dpjson)
-  t.deepEqual(exp, res)
-})
-
-test('Gets correct file info for request', t => {
-  const files = ['test/fixtures/datapackage.json', 'README.md', 'test/fixtures/sample.csv']
-  const res = push.getFilesForRequest(files, 'publisher', 'package')
-  const exp = {
-    filedata: {
-      "README.md": {
-        md5: "WCPiBZTZssO/uTd4IT/X0w==",
-        name: "README.md",
-        length: 1018,
-        type: "binary/octet-stream",
-      },
-      "test/fixtures/datapackage.json": {
-        md5: "hfsvgF7g9q6VsBAv63zB0w==",
-        name: "test/fixtures/datapackage.json",
-        length: 712,
-        type: "application/json",
-      },
-      "test/fixtures/sample.csv": {
-        md5: "sGYdlWZJioAPv5U2XOKHRw==",
-        name: "test/fixtures/sample.csv",
-        length: 46,
-        type: "binary/octet-stream",
-      },
-    },
-    metadata: {
-      name: "package",
-      owner: "publisher",
-    },
-  }
-  t.deepEqual(exp, res)
-})
-*/
-
-/*
-test('uploads spec into spec store', async t => {
-  const resources = [
-    {
-      'name': 'file1'
-    },
-    {
-      'name': 'file2'
-    }
-  ]
-  const datafile = {
-    'datapackage.json': {
-      'md5': 'hash1',
-      'upload_url': 'http://test.com'
-    },
-    'file1': {
-      'md5': 'hash2'
-    },
-    'file2': {
-      'md5': 'hash3'
-    }
-  }
-  const out = await push.uploadToSpecStore(config, datafile, resources)
-  t.is(out.success, true)
-})
-*/
