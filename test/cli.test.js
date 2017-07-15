df4b916e3fc05b4e0c218102735364e96fc70607
Rufus Pollock <rufus.pollock@okfn.org>
2017-07-15 06:40:04 +0100

    [test][xs]: move test_args-parsing.js (tests version and help) into cli.test.js.

--- test/cli.test.js
+++ test/cli.test.js
@@ -4,6 +4,9 @@ const path = require('path')
 const test = require('ava')
 const { spawn } = require('cross-spawn')
 
+const { version } = require('../package.json')
+
+
 const runcli = (...args) => {
   return new Promise((resolve, reject) => {
     const command = path.resolve(__dirname, '../bin/data.js')
@@ -27,6 +30,35 @@ const runcli = (...args) => {
   })
 }
 
+
+// ==========
+// The basics
+
+
+test('"data -v --version" prints version', async t => {
+  let result = await runcli('-v')
+
+  t.is(result.code, 0)
+  let stdout = result.stdout.split('\n')
+  t.true(stdout.length > 1)
+  t.true(stdout[0].includes(`Version: ${version}`))
+
+  result = await runcli('--version')
+
+  t.is(result.code, 0)
+  stdout = result.stdout.split('\n')
+  t.true(stdout.length > 1)
+  t.true(stdout[0].includes(`Version: ${version}`))
+})
+
+test('"data help" prints help message', async t => {
+  const result = await runcli('help')
+
+  t.is(result.code, 0)
+  const stdout = result.stdout.split('\n')
+  t.true(stdout.length > 1)
+  t.true(stdout[1].includes('  ❒ data [options] <command> <args>'))
+})
 
 // Push
 
