3d98305a6a2ba3520de1c3fc06dfb363a4b3d265
anuveyatsu <anuar.ustayev@gmail.com>
2017-09-07 13:32:03 +0600

    [push][xs]: when pushing a single file, it adds schema only for csv format

--- bin/data-push.js
+++ bin/data-push.js
@@ -89,18 +89,19 @@ Promise.resolve().then(async () => {
 const prepareDatasetFromFile = async filePath => {
   const pathParts = path.parse(filePath)
   const file = File.load(pathParts.base, {basePath: pathParts.dir})
+  if (file.format === 'csv') {
+    await file.addSchema()
+    const headers = file.descriptor.schema.fields.map(field => field.name)
+    const fieldTypes = file.descriptor.schema.fields.map(field => field.type)
+    if (argv.interactive) {
+      // Prompt user with headers and fieldTypes
+      const questions = [ask('headers', headers), ask('types', fieldTypes)]
+      const answers = await inquirer.prompt(questions)
 
-  await file.addSchema()
-  const headers = file.descriptor.schema.fields.map(field => field.name)
-  const fieldTypes = file.descriptor.schema.fields.map(field => field.type)
-  if (argv.interactive) {
-    // Prompt user with headers and fieldTypes
-    const questions = [ask('headers', headers), ask('types', fieldTypes)]
-    const answers = await inquirer.prompt(questions)
-
-    if (answers.headers === 'n' & answers.types === 'n') {
-      // Maybe nicer exit - user has chosen not to proceed for now ...
-      throw new Error('Please, generate datapackage.json and push.')
+      if (answers.headers === 'n' & answers.types === 'n') {
+        // Maybe nicer exit - user has chosen not to proceed for now ...
+        throw new Error('Please, generate datapackage.json and push.')
+      }
     }
   }
 
