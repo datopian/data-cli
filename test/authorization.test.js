6095d3bfc87fa4f2f166f2d86a4fb513e05be654
anuveyatsu <anuar.ustayev@gmail.com>
2018-05-18 14:03:28 +0600

    [test][s]: use datahub-client provided Agent instead of helpers for getting proper status code

--- test/authorization.test.js
+++ test/authorization.test.js
@@ -1,31 +1,56 @@
 const test = require('ava')
-const whatStatusCode = require('../lib/utils/helpers')
+const {Agent} = require('datahub-client')
 
 // ==========================
 // USER RIGHTS & RESTRICTIONS
 
-const path = 'https://datahub.io/anuveyatsu/finance-vix'
-const dpJsonPath = path + '/datapackage.json'
-const resourceCsvPath = path + '/r/vix-daily.csv'
-const resourceJsonPath = path + '/r/vix-daily.json'
-const zipPath = path + '/r/finance-vix_zip.zip'
+const mainPath = '/anuveyatsu/finance-vix'
+const dpJsonPath = mainPath + '/datapackage.json'
+const resourceCsvPath = mainPath + '/r/vix-daily.csv'
+const resourceJsonPath = mainPath + '/r/vix-daily.json'
+const zipPath = mainPath + '/r/finance-vix_zip.zip'
 
-test.failing('Access private dataset as unauthorized user', async t => {
-  t.is(await whatStatusCode(path), 404)
-  t.is(await whatStatusCode(dpJsonPath), 404)
-  t.is(await whatStatusCode(resourceCsvPath), 404)
-  t.is(await whatStatusCode(resourceJsonPath), 404)
-  t.is(await whatStatusCode(zipPath), 404)
+const agent = new Agent('https://datahub.io')
+
+test('Access private dataset as unauthorized user', async t => {
+  let response = await agent.fetch(mainPath)
+  t.is(response.status, 404)
+  response = await agent.fetch(dpJsonPath)
+  t.is(response.status, 404)
+  response = await agent.fetch(resourceCsvPath)
+  t.is(response.status, 404)
+  response = await agent.fetch(resourceJsonPath)
+  t.is(response.status, 404)
+  response = await agent.fetch(zipPath)
+  t.is(response.status, 404)
 })
 
-test.failing('Access private dataset as non-owner user', async t => {
+test('Access private dataset as non-owner user', async t => {
   // Token for 'test' user (Travis knows it):
   const token = process.env.token
-  t.is(await whatStatusCode(path + `?jwt=${token}`), 404)
-  t.is(await whatStatusCode(dpJsonPath + `?jwt=${token}`), 404)
-  t.is(await whatStatusCode(resourceCsvPath + `?jwt=${token}`), 404)
-  t.is(await whatStatusCode(resourceJsonPath + `?jwt=${token}`), 404)
-  t.is(await whatStatusCode(zipPath + `?jwt=${token}`), 404)
+  let response = await agent.fetch(mainPath + `?jwt=${token}`)
+  t.is(response.status, 404)
+  response = await agent.fetch(dpJsonPath + `?jwt=${token}`)
+  t.is(response.status, 404)
+  response = await agent.fetch(resourceCsvPath + `?jwt=${token}`)
+  t.is(response.status, 404)
+  response = await agent.fetch(resourceJsonPath + `?jwt=${token}`)
+  t.is(response.status, 404)
+  response = await agent.fetch(zipPath + `?jwt=${token}`)
+  t.is(response.status, 404)
 })
 
-// P.S. the dataset was tested with owner account and returned non-404 status code
+test('Access private dataset as owner', async t => {
+  // Owner's token is stored as secret env var on Travis
+  const token = process.env.SECRET_OWNER_TOKEN
+  let response = await agent.fetch(mainPath + `?jwt=${token}`)
+  t.is(response.status, 200)
+  response = await agent.fetch(dpJsonPath + `?jwt=${token}`)
+  t.is(response.status, 200)
+  response = await agent.fetch(resourceCsvPath + `?jwt=${token}`)
+  t.is(response.status, 200)
+  response = await agent.fetch(resourceJsonPath + `?jwt=${token}`)
+  t.is(response.status, 200)
+  response = await agent.fetch(zipPath + `?jwt=${token}`)
+  t.is(response.status, 200)
+})
